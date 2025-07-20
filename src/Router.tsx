001d8003f63f0f3753a8d29783d4d13a5dbfb58a
Jozsef Frigo <frigojozsef@gmail.com>
2025-07-20 16:59:49 +0200

    use HashRouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,20 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { TechnologyPage } from './pages/Technology.page';
 import { TodolistPage } from './pages/Todolist.page';
+import { Error404Page } from './pages/Error.404.page';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: '/',
+    path: '/technology',
     element: <TechnologyPage />,
   },
   {
     path: '/list',
     element: <TodolistPage />,
+  },
+  {
+    path: '*',
+    element: <Error404Page />,
   },
 ]);
 
