d6daa560f3d2ca0eab4dedca0a5a5586ed1ccc70
Jozsef Frigo <frigojozsef@gmail.com>
2025-07-21 21:25:24 +0200

    add checkboxes

--- src/components/Todolist/Todolist.tsx
+++ src/components/Todolist/Todolist.tsx
@@ -1,21 +1,36 @@
 import '@mantine/core/styles.layer.css';
 import 'mantine-datatable/styles.layer.css';
 import { DataTable } from 'mantine-datatable';
-import { comment } from 'postcss';
 
 const data = [
-  { id: 1, title: 'set up table columns',
+  { id: 1, title: 'set up table columns', done: true, 
     comments: 'We start with title and comments'
   },
-  { id: 2, title: 'add checkboxes', 
-    comments: `ops, there are two functions for checkboxes
+  { id: 2, title: 'add checkboxes', done: true, 
+    comments: `Ops, there are two functions for checkboxes
     - multi-select: allows selecting multiple rows
     - task done: allows marking a task as done` 
   },
+  { id: 3, title: 'make the checkbox clickable', done: false, 
+    comments: `figure out how to make the checkbox clickable 
+    and toggle the done state` 
+  },
 ];
+
+type Todo = {
+  id: number;
+  title: string;
+  done: boolean;
+  comments: string;
+};
 
 const columns = [
   { accessor: 'id', title: 'ID' },
+  { accessor: 'done', title: 'Done', 
+    render: ({ done }: Pick<Todo, 'done'>) => (
+      <input type="checkbox" checked={done} readOnly /> 
+    ),
+  },
   { accessor: 'title', title: 'Title' },
   { accessor: 'comments', title: 'Comments' },
 ];
