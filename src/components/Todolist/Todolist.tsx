456f1b71070c48c45c7f7132a3691bbca8e547b4
Jozsef Frigo <frigojozsef@gmail.com>
2025-07-21 19:46:42 +0200

    add sample tasks to the table

--- src/components/Todolist/Todolist.tsx
+++ src/components/Todolist/Todolist.tsx
@@ -1,16 +1,23 @@
 import '@mantine/core/styles.layer.css';
 import 'mantine-datatable/styles.layer.css';
 import { DataTable } from 'mantine-datatable';
+import { comment } from 'postcss';
 
 const data = [
-  { id: 1, name: 'Alice', age: 28 },
-  { id: 2, name: 'Bob', age: 34 },
+  { id: 1, title: 'set up table columns',
+    comments: 'We start with title and comments'
+  },
+  { id: 2, title: 'add checkboxes', 
+    comments: `ops, there are two functions for checkboxes
+    - multi-select: allows selecting multiple rows
+    - task done: allows marking a task as done` 
+  },
 ];
 
 const columns = [
   { accessor: 'id', title: 'ID' },
-  { accessor: 'name', title: 'Name' },
-  { accessor: 'age', title: 'Age' },
+  { accessor: 'title', title: 'Title' },
+  { accessor: 'comments', title: 'Comments' },
 ];
 
 export default function Todolist() {
