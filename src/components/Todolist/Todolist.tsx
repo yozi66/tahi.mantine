4e38eeb759a4ce023adccaa32c0a17dbb3237d90
Jozsef Frigo <frigojozsef@gmail.com>
2025-07-22 20:30:28 +0200

    wip: editor for title

--- src/components/Todolist/Todolist.tsx
+++ src/components/Todolist/Todolist.tsx
@@ -2,40 +2,52 @@ import '@mantine/core/styles.layer.css';
 import 'mantine-datatable/styles.layer.css';
 import { DataTable, DataTableColumn } from 'mantine-datatable';
 import { useState } from 'react';
+import { Text } from '@mantine/core';
 
 const data = [
-  { id: 1, title: 'set up table columns', done: true, 
+  { id: 1, title: { value: 'set up table columns'}, done: true, 
     comments: 'We start with title and comments'
   },
-  { id: 2, title: 'add checkboxes', done: true, 
+  { id: 2, title: { value: 'add checkboxes'}, done: true, 
     comments: `Ops, there are two functions for checkboxes
     - multi-select: allows selecting multiple rows
     - task done: allows marking a task as done` 
   },
-  { id: 3, title: 'make the checkbox clickable', done: true, 
+  { id: 3, title: { value: 'make the checkbox clickable'}, done: true, 
     comments: `figure out how to make the checkbox clickable 
     and toggle the done state` 
   },
-  { id: 4, title: 'prevent line breaks', done: true, 
+  { id: 4, title: { value: 'prevent line breaks'}, done: true, 
     comments: `prevent line breaks in the table cells
     - use ellipsis for overflow text
     - help: https://icflorescu.github.io/mantine-datatable/examples/column-properties-and-styling/` 
   },
-  { id: 5, title: 'electron prototype', done: false, 
+  { id: 5, title: { value: 'electron prototype'}, done: false, 
     comments: `prepare for load and save of the todo list
     - use localStorage for persistence
     - use electron for desktop app`
   },
-  { id: 6, title: 'define the concept for editing todos', done: false, 
+  { id: 6, title: { value: 'define the concept for editing todos'}, done: true, 
     comments: `decide how to edit todos, in place or below the table
-    - in place: edit the title and comments directly in the table
-    - below the table: show a form to edit the selected todo`
+    - in place: edit the title directly in the table
+    - clicking a row copies the comments to the textarea below the table
+    - editing the comments in the textarea updates the table
+    `
+  },
+  { id: 7, title: { value: 'in-place editor for the title'}, done: false, 
+    comments: `implement an in-place editor for the title
+    - clicking the title opens an input field`
   },
 ];
 
+type EditableString = {
+  value: string;
+  editing?: boolean;
+};
+
 type Todo = {
   id: number;
-  title: string;
+  title: EditableString;
   done: boolean;
   comments: string;
 };
@@ -47,16 +59,37 @@ const columns = [
       <input type="checkbox" checked={done} readOnly /> 
     ),
   },
-  { accessor: 'title', title: 'Title', ellipsis: true,},
+  { accessor: 'title', title: 'Title',
+    render: ({ title }: Pick<Todo, 'title'>) => {
+      return title.editing ? (
+        <input 
+          type="text" 
+          value={title.value} 
+          style={{ width: '100%' }} 
+        />
+      ) : (
+        <Text truncate='end' size='sm'>{title.value}</Text>
+      );
+    },
+  },
   { accessor: 'comments', title: 'Comments', ellipsis: true,},
 ];
 
 export default function Todolist() {
   const [todoData, setTodoData] = useState(data);
 
-  const updateTodoData = (record: Todo, column: DataTableColumn<Todo>) => {
+  const handleCellClick = (record: Todo, column: DataTableColumn<Todo>) => {
+    // Toggle edit mode when the title is clicked
+    if (column.accessor === 'title') {
+      const updatedData = todoData.map((todo) => 
+        todo.id === record.id 
+          ? { ...todo, title: {value: todo.title.value, editing: true} }
+          : { ...todo, title: {value: todo.title.value, editing: false} }
+      );
+      setTodoData(updatedData);
+    }
+    // Toggle the done state when the checkbox is clicked
       if (column.accessor === 'done') {
-      // Toggle the done state when the checkbox is clicked
       const updatedData = todoData.map((todo) => 
         todo.id === record.id ? { ...todo, done: !todo.done } : todo
       );
@@ -72,7 +105,7 @@ export default function Todolist() {
       striped
       highlightOnHover
       onCellClick={({record, column}) => {
-        updateTodoData(record as Todo, column);
+        handleCellClick(record as Todo, column);
         }
       }
     />
